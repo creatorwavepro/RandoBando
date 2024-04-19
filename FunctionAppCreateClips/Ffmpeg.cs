48fcecf885c22dbd724add7f88174e0ef26610b0
ardab <ardab@VICTUSARDA>
2024-04-19 14:49:42 +0200

    check if works

--- FunctionAppCreateClips/Ffmpeg.cs
+++ FunctionAppCreateClips/Ffmpeg.cs
@@ -80,7 +80,7 @@ namespace FunctionAppCreateClips
 
             if (!string.IsNullOrEmpty(path) && path.Contains("http"))
             {
-                return new OkObjectResult(path);
+                return new OkObjectResult("https://blitzstoragewesteurope.blob.core.windows.net/finalfiles/clip-18c8d406-8509-45d3-9871-989896ab840e.mp4?sp=r&st=2024-04-19T12:48:37Z&se=2024-05-11T20:48:37Z&spr=https&sv=2022-11-02&sr=b&sig=x1v5uZ6gD70E%2BdNJEviDXfVmesrpmi79g7mRPNK4OiU%3D");
             }
             else
             {
